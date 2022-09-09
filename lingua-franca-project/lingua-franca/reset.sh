e7d67bfffe3301e0fc03548e1ac965968e5c0943
purdyjen <purdyjen@me.com>
2022-09-09 10:55:48 -0400

    restructured project, created reset script

--- lingua-franca-project/lingua-franca/reset.sh
+++ lingua-franca-project/lingua-franca/reset.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Don't forget to run "chmod +x reset.sh" to give the script permissions
+# Reference:
+# https://www.gnu.org/software/bash/manual/html_node/Filename-Expansion.html
 
 GLOBIGNORE="*.sh" # The script will now ignore any files that match this pattern
 rm -R ./*
