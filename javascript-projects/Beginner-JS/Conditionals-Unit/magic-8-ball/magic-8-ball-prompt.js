4c447c3ec9e59585bca5b622c50a69547005f490
purdyjen <purdyjen@me.com>
2022-10-10 10:43:11 -0400

    added package-json

--- javascript-projects/Beginner-JS/Conditionals-Unit/magic-8-ball/magic-8-ball-prompt.js
+++ javascript-projects/Beginner-JS/Conditionals-Unit/magic-8-ball/magic-8-ball-prompt.js
@@ -4,15 +4,15 @@ https://www.codecademy.com/learn/introduction-to-javascript
 */
 
 // Adds prompt functionality for node
-// to install, type "npm install" (if you downloaded the package.json) or "npm -i prompt-sync"
-const prompt = require('prompt-sync')();
+// to install, type "npm install" (if you downloaded the package.json) or "npm install prompt-sync"
+const prompt = require("prompt-sync")();
 
 // defines a variable (userName) and prompts the user for their name
 let userName = prompt("What is your name? ");
 
 // a ternary expression that decides how to greet the user based on whether or not the user has entered a name
-  // format: 
-  // conditionToCheck ? code if truthy : code if falsy
+// format:
+// conditionToCheck ? code if truthy : code if falsy
 userName ? console.log(`Hello, ${userName}!`) : console.log("Hello!");
 
 // prompts the user for their desired question
@@ -22,33 +22,33 @@ const userQuestion = prompt("You may ask the Magic 8 Ball your question: ");
 let randomNumber = Math.floor(Math.random() * 8);
 
 // creates a variable to store the Magic 8 Ball's response to the question
-let eightBall = '';
+let eightBall = "";
 
 // the core logic of the Magic 8 Ball program. Takes the number randomly generated and assigns the corresponding response to the eightBall variable
 switch (randomNumber) {
   case 0:
-    eightBall = 'It is certain.';
+    eightBall = "It is certain.";
     break;
   case 1:
-    eightBall = 'It is decidedly so.';
+    eightBall = "It is decidedly so.";
     break;
   case 2:
-    eightBall = 'Reply hazy. Try again.';
+    eightBall = "Reply hazy. Try again.";
     break;
   case 3:
-    eightBall = 'Cannot predict now.';
+    eightBall = "Cannot predict now.";
     break;
   case 4:
-    eightBall = 'Do not count on it.';
+    eightBall = "Do not count on it.";
     break;
   case 5:
-    eightBall = 'My sources say no.';
+    eightBall = "My sources say no.";
     break;
   case 6:
-    eightBall = 'Outlook not so good.';
+    eightBall = "Outlook not so good.";
     break;
   case 7:
-    eightBall = 'Signs point to yes.';
+    eightBall = "Signs point to yes.";
     break;
   default:
     break;
