208f3d421be359db7bc960ddf73fe76acc2ac778
purdyjen <purdyjen@me.com>
2022-10-14 09:04:21 -0400

    minor edits to arrays project

--- javascript-projects/Beginner-JS/Arrays-Unit/secret-message.js
+++ javascript-projects/Beginner-JS/Arrays-Unit/secret-message.js
@@ -3,8 +3,12 @@ From Codecademy's Learn JavaScript course, Arrays Unit:
 https://www.codecademy.com/learn/introduction-to-javascript
 */
 
+/*
+Overview: Using array methods, you will transform an array of strings into a secret message!
+*/
+
+
 let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
-// console.log(secretMessage.length);
 
 //1. Use an array method to remove the last string of the array secretMessage.
 secretMessage.pop();
